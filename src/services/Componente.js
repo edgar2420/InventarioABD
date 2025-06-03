787b8cfc43d87f04cb444ccd0cac5a3b49572983
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-06-03 15:48:16 -0400

    Update Componente.js

--- src/services/Componente.js
+++ src/services/Componente.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:3000/api'
+const API_BASE = 'https://backendinventario-dlaq.onrender.com/api'
 
 // 📦 Obtener todos los componentes
 export const obtenerComponentes = async () => {
